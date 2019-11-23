7ea31212c3e4eed7864df52c68ceeb756fc8731d
kakaemma <kakaemma1@gmail.com>
2019-11-23 15:45:05 +0300

    feat(caching) :add caching - add redis - cache vat validation - cache currency converting

--- app/controllers/current_time_controller.rb
+++ app/controllers/current_time_controller.rb
@@ -1,5 +1,9 @@
+# app/controllers/current_time_controller.rb
+
+# Current time controller
 class CurrentTimeController < ApplicationController
-  include CurrentTimeHelper
+  include CurrentTimeControllerHelper
+  # get current time and return json
   def index
     json_response(current_time)
   end
---
+++ app/controllers/helpers/current_time_controller_helper.rb
@@ -0,0 +1,6 @@
+# Module CurrentTimeControllerHelper
+module CurrentTimeControllerHelper
+  def current_time
+    { current_time: Time.now }
+  end
+end
